08427367b45f8c435561d53730c1f370244c03e3
Jonathan Palafoutas <jpalafou@princeton.edu>
2024-12-03 14:23:04 -0500

    refactor Stencil class to support flexible positions

--- stencilpal/stencil.py
+++ stencilpal/stencil.py
@@ -1,3 +1,4 @@
+import warnings
 from numbers import Number
 from typing import Tuple, Union
 
@@ -16,26 +17,38 @@ class Stencil:
 
     def __init__(
         self,
-        x: Union[list, tuple, np.ndarray],
-        w: Union[list, tuple, np.ndarray, rp.RationalArray],
-        h: int = 1,
+        x: Union[list, tuple, np.ndarray] = (),
+        w: Union[list, tuple, np.ndarray, rp.RationalArray] = (),
     ):
         """
         Args:
-            x: Union[list, tuple, np.ndarray] - stencil positions, must be integers
-            w: Union[list, tuple, np.ndarray, rp.RationalArray] - stencil weights
-            h: int - step size between stencil positions
+            x: Union[list, tuple, np.ndarray] - stencil positions, must be non-empty,
+                1D, non-repeating, and integers
+            w: Union[list, tuple, np.ndarray, rp.RationalArray] - stencil weights, must
+                be non-empty and 1D
         """
-        self.x = np.array(x)
-        self.w = np.array(w) if not isinstance(w, rp.RationalArray) else w
-        self.h = h
+        # assign x attribute
+        if isinstance(x, list) or isinstance(x, tuple):
+            self.x = np.array([], int) if len(x) == 0 else np.array(x)
+        elif isinstance(x, np.ndarray):
+            self.x = x
+        else:
+            raise ValueError(f"Unsupported type {type(x)} for x.")
+
+        # assign w attribute
+        if isinstance(w, list) or isinstance(w, tuple):
+            self.w = np.array([], float) if len(w) == 0 else np.array(w)
+        elif isinstance(w, np.ndarray) or isinstance(w, rp.RationalArray):
+            self.w = w
+        else:
+            raise ValueError(f"Unsupported type {type(w)} for w.")
 
         # validate and format
         self._validate_and_format()
 
     def _validate_and_format(self):
         if not np.issubdtype(self.x.dtype, np.integer):
-            raise ValueError("x must be integers.")
+            raise ValueError(f"x must be integers, but got {self.x.dtype}.")
         if self.x.ndim != 1 or self.w.ndim != 1:
             raise ValueError("x and w must be 1D arrays.")
         if self.x.size != self.w.size:
@@ -43,14 +56,22 @@ class Stencil:
         if len(np.unique(self.x)) != len(self.x):
             raise ValueError("x must be unique.")
 
+        # store attributes
+        self.rational = isinstance(self.w, rp.RationalArray)
+        self.size = self.x.size
+
         # sort x and w by x
         if not np.all(np.diff(self.x) > 0):
             sorted_indices = np.argsort(self.x)
             self.x, self.w = self.x[sorted_indices], self.w[sorted_indices]
 
-        # store attributes
-        self.rational = isinstance(self.w, rp.RationalArray)
-        self.size = self.x.size
+    def _check_uniform_spacing(self) -> bool:
+        """
+        Checks if the stencil positions are uniformly spaced.
+        """
+        if self.size < 2:
+            return True
+        return np.all(np.diff(self.x) == np.diff(self.x)[0])
 
     def __repr__(self) -> str:
         return f"Stencil(x={self.x}, w={self.w}, rational={self.rational})"
@@ -58,62 +79,139 @@ class Stencil:
     def __str__(self) -> str:
         return self.__repr__()
 
+    def add_node(
+        self, x: int, w: Union[float, rp.RationalArray] = 1, inplace: bool = True
+    ) -> Union[None, "Stencil"]:
+        """
+        Adds a node to the stencil.
+
+        Args:
+            x: int - position of the new node
+            w: Union[float, rp.RationalArray] - weight of the new node
+            inplace: bool - if True, modify the stencil in place. If False, return a
+                new stencil.
+
+        Returns:
+            None or Stencil - None if inplace is True, otherwise a new stencil
+        """
+        if not np.issubdtype(type(x), np.integer):
+            raise ValueError(f"x must be an integer, but got {type(x)}.")
+        if not (isinstance(w, float) or isinstance(w, rp.RationalArray)):
+            raise ValueError(
+                f"w must be a float or rp.RationalArray, but got {type(w)}."
+            )
+        _x, _w = np.append(self.x, x), np.append(self.w, w)
+        if inplace:
+            self.x = _x
+            self.w = _w
+            self._validate_and_format()
+        else:
+            return Stencil(_x, _w)
+
+    def remove_node(self, x: int, inplace: bool = True) -> Union[None, "Stencil"]:
+        """
+        Removes a node from the stencil.
+
+        Args:
+            x: int - position of the node to remove
+            inplace: bool - if True, modify the stencil in place. If False, return a
+                new stencil.
+
+        Returns:
+            None or Stencil - None if inplace is True, otherwise a new stencil
+        """
+        if self.rational:
+            raise NotImplementedError("Cannot remove node from rational stencil.")
+        if not np.issubdtype(type(x), np.integer):
+            raise ValueError(f"x must be an integer, but got {type(x)}.")
+        if x not in self.x:
+            raise ValueError(f"Node at position {x} not found in stencil.")
+        _x, _w = (
+            np.delete(self.x, np.where(self.x == x)),
+            np.delete(self.w, np.where(self.x == x)),
+        )
+        if inplace:
+            self.x = _x
+            self.w = _w
+            self._validate_and_format()
+        else:
+            return Stencil(_x, _w)
+
     def copy(self) -> "Stencil":
         """
         Returns a copy of the stencil.
         """
-        return Stencil(self.x.copy(), self.w.copy(), self.h)
+        return Stencil(self.x.copy(), self.w.copy())
 
     def rescope(
-        self, xl: int = None, xr: int = None, h: float = None, inplace: bool = True
+        self, x: np.ndarray = None, h: int = 1, inplace: bool = True
     ) -> Union[None, "Stencil"]:
         """
-            Rescopes the stencil to the interval [xl, xr] with step size h. Fills in zeros
-                for weights at missing positions.
+        Rescopes the stencil to new positions defined by x. Fills in zeros for weights
+            at missing positions.
 
         Args:
-            xl: int - left boundary of the stencil
-            xr: int - right boundary of the stencil
-            h: float - step size between stencil positions
+            x: np.ndarray - new stencil positions. If None, the stencil is rescoped to
+                the range of existing positions uniformly spaced by h.
+            h: int - step size between stencil positions used when x is None.
             inplace: bool - if True, modify the stencil in place. If False, return a
                 new stencil.
 
-            Returns:
-                None or Stencil - None if inplace is True, otherwise a new stencil
+        Returns:
+            None or Stencil - None if inplace is True, otherwise a new stencil
         """
-        xl = self.x.min() if xl is None else xl
-        xr = self.x.max() if xr is None else xr
-        if xl > xr:
-            raise StencilError("xl must be less than or equal to xr.")
-        if xl > self.x.min() or xr < self.x.max():
-            raise StencilError("Rescoping cannot clip existing stencil positions.")
-        _h = self.h if h is None else h
-        _x = np.arange(xl, xr + 1, _h)
+        if self.size < 2 and x is None:
+            raise ValueError(
+                "Cannot rescope stencil with less than 2 positions without specifying x."
+            )
 
-        # check if there are any self.x not in _x
-        if not np.all(np.isin(self.x, _x)):
-            raise StencilError("Rescoping would exclude existing stencil positions.")
+        _x = np.arange(self.x[0], self.x[-1] + 1, h) if x is None else x
+
+        if not np.all(np.isin(self.x[np.nonzero(self.w)[0]], _x)):
+            raise StencilError(
+                "Rescoping would exclude existing stencil positions with non-zero weights."
+            )
 
         _w = (
-            np.zeros_like(_x, dtype=self.w.dtype)
-            if not self.rational
-            else rp.rational_array(np.zeros_like(_x))
+            rp.rational_array(np.zeros_like(_x))
+            if self.rational
+            else np.zeros_like(_x, dtype=self.w.dtype)
         )
-        _w[np.searchsorted(_x, self.x)] = self.w
-
-        # ensure that the sum of the absolute values of the weights is the same
-        if self.rational and np.sum(np.abs(_w)) != np.sum(np.abs(self.w)):
-            raise StencilError("Weight abs sum mismatch.")
+        if not np.all(self.w == 0):
+            _w[np.searchsorted(_x, self.x)] = self.w
 
         if inplace:
-            self.x, self.w, self.h = _x, _w, _h
+            self.x, self.w = _x, _w
             self._validate_and_format()
         else:
-            return Stencil(_x, _w, _h)
+            return Stencil(_x, _w)
 
-    def trim_zeros(self, inplace: bool = True) -> Union[None, "Stencil"]:
+    def _trim_helper(self, f: callable, inplace: bool = True) -> Union[None, "Stencil"]:
         """
-        Removes zeros from the stencil.
+        Helper function for trimming zeros from the stencil.
+
+        Args:
+            f: callable - function to apply to non-zero indices. Should take an array
+                of non-zero indices and return a tuple of new x and w arrays.
+                f(non_zero_indices) -> (new_x, new_w)
+            inplace: bool - if True, modify the stencil in place. If False, return a
+                new stencil.
+        """
+        non_zero_indices = np.nonzero(self.w)[0]
+        _x, _w = (
+            f(non_zero_indices)
+            if non_zero_indices.size != 0
+            else (np.array([], dtype=self.x.dtype), np.array([], dtype=self.w.dtype))
+        )
+        if inplace:
+            self.x, self.w = _x, _w
+            self._validate_and_format()
+        else:
+            return Stencil(_x, _w)
+
+    def trim_all_zeros(self, inplace: bool = True) -> Union[None, "Stencil"]:
+        """
+        Removes all stencil nodes at which the weight is zero.
 
         Args:
             inplace: bool - if True, modify the stencil in place. If False, return a
@@ -122,23 +220,19 @@ class Stencil:
         Returns:
             None or Stencil - None if inplace is True, otherwise a new stencil
         """
-        non_zero_idx = np.nonzero(self.w)[0]
-        if non_zero_idx.size == 0:
-            raise ValueError("Cannot trim all zeros.")
-        _x = self.x[non_zero_idx]
-        _w = self.w[non_zero_idx]
-        _h = self.h
-        if inplace:
-            self.x, self.w, self.h = _x, _w, _h
-            self._validate_and_format()
-        else:
-            return Stencil(_x, _w, _h)
+        return self._trim_helper(
+            lambda non_zero_indices: (
+                self.x[non_zero_indices],
+                self.w[non_zero_indices],
+            ),
+            inplace,
+        )
 
     def trim_leading_and_trailing_zeros(
         self, inplace: bool = True
     ) -> Union[None, "Stencil"]:
         """
-        Removes leading and trailing zeros from the stencil.
+        Removes leading and trailing zero-weight nodes from the stencil.
 
         Args:
             inplace: bool - if True, modify the stencil in place. If False, return a
@@ -147,18 +241,13 @@ class Stencil:
         Returns:
             None or Stencil - None if inplace is True, otherwise a new stencil
         """
-        non_zero_indices = np.nonzero(self.w)[0]
-        if non_zero_indices.size == 0:
-            raise ValueError("Cannot trim all zeros.")
-        start, end = non_zero_indices[0], non_zero_indices[-1] + 1
-        _x = self.x[start:end]
-        _w = self.w[start:end]
-        _h = self.h
-        if inplace:
-            self.x, self.w, self.h = _x, _w, _h
-            self._validate_and_format()
-        else:
-            return Stencil(_x, _w, _h)
+        return self._trim_helper(
+            lambda non_zero_indices: (
+                self.x[non_zero_indices[0] : non_zero_indices[-1] + 1],
+                self.w[non_zero_indices[0] : non_zero_indices[-1] + 1],
+            ),
+            inplace,
+        )
 
     def simplify(self, inplace: bool = True) -> Union[None, "Stencil"]:
         """
@@ -174,6 +263,8 @@ class Stencil:
         stencil = self if inplace else self.copy()
         if self.rational:
             stencil.w.simplify()
+        else:
+            warnings.warn("Cannot simplify non-rational stencil.")
         return None if inplace else stencil
 
     def form_common_denominator(self, inplace: bool = True) -> Union[None, "Stencil"]:
@@ -190,37 +281,41 @@ class Stencil:
         stencil = self if inplace else self.copy()
         if self.rational:
             stencil.w.form_common_denominator()
+        else:
+            warnings.warn("Cannot form common denominator for non-rational stencil.")
         return None if inplace else stencil
 
-    def asnumpy(self, mode: str = "numerator") -> np.ndarray:
+    def asnumpy(
+        self, mode: str = "numerator", trim_leading_and_trailing_zeros: bool = True
+    ) -> np.ndarray:
         """
         Returns the weights of the stencil as a numpy array.
 
         Args:
             mode: str - 'numerator' or 'float'
                 'numerator' - returns the numerator of the rational weights after
-                forming a common denominator
+                forming a common denominator. Only valid for rational stencils.
                 'float' - returns the weights as a float array
+            trim_leading_and_trailing_zeros: bool - if True, trim leading and trailing
+                zeros from the stencil before returning the weights
 
         Returns:
             np.ndarray - the weights of the stencil
         """
+        if not self._check_uniform_spacing():
+            raise ValueError("Cannot convert non-uniform stencil to numpy array.")
         if mode == "numerator":
             if not self.rational:
                 raise ValueError(
                     "'numerator' mode can only be used with rational stencils."
                 )
             stencil = self.simplify(inplace=False)
-            stencil.rescope(inplace=True)
             stencil.form_common_denominator(inplace=True)
             return stencil.w.numerator
         elif mode == "float":
             stencil = self.rescope(inplace=False)
             return np.array(stencil.w)
         raise ValueError(f"Unsupported mode {mode}.")
-
-    def __neg__(self) -> "Stencil":
-        return Stencil(self.x, -self.w, self.h)
 
     def _align_with(self, other: "Stencil") -> Tuple["Stencil", "Stencil"]:
         """
@@ -229,35 +324,18 @@ class Stencil:
         Returns:
             Tuple[Stencil, Stencil] - the aligned stencils
         """
-        xl, xr = min(self.x.min(), other.x.min()), max(self.x.max(), other.x.max())
-        _h = min(self.h, other.h)
-        rescope1 = self.rescope(xl, xr, _h, inplace=False)
-        rescope2 = other.rescope(xl, xr, _h, inplace=False)
-        if not np.all(rescope1.x == rescope2.x):
-            raise StencilError("Failed to align stencils.")
+        _x = np.union1d(self.x, other.x)
+        rescope1 = self.rescope(_x, inplace=False)
+        rescope2 = other.rescope(_x, inplace=False)
         return rescope1, rescope2
 
-    def __mul__(
-        self, other: Union["Stencil", rp.RationalArray, np.ndarray, Number]
-    ) -> "Stencil":
-        if isinstance(other, Stencil):
-            rescope1, rescope2 = self._align_with(other)
-            return Stencil(rescope1.x, rescope1.w * rescope2.w, rescope1.h)
-        elif (
-            isinstance(other, rp.RationalArray)
-            or isinstance(other, np.ndarray)
-            or isinstance(other, Number)
-        ):
-            return Stencil(self.x, self.w * other, self.h)
-        raise ValueError(f"Unsupported operand type {type(other)} for *.")
-
-    def __rmul__(self, other: Union[Number, rp.RationalArray]) -> "Stencil":
-        return self * other
+    def __neg__(self) -> "Stencil":
+        return Stencil(self.x, -self.w)
 
     def __add__(self, other: "Stencil") -> "Stencil":
         if isinstance(other, Stencil):
             rescope1, rescope2 = self._align_with(other)
-            return Stencil(rescope1.x, rescope1.w + rescope2.w, rescope1.h)
+            return Stencil(rescope1.x, rescope1.w + rescope2.w)
         raise ValueError(f"Unsupported operand type {type(other)} for +.")
 
     def __radd__(self, other: "Stencil") -> "Stencil":
@@ -265,3 +343,20 @@ class Stencil:
 
     def __sub__(self, other: "Stencil") -> "Stencil":
         return self + -other
+
+    def __mul__(
+        self, other: Union["Stencil", rp.RationalArray, np.ndarray, Number]
+    ) -> "Stencil":
+        if isinstance(other, Stencil):
+            rescope1, rescope2 = self._align_with(other)
+            return Stencil(rescope1.x, rescope1.w * rescope2.w)
+        elif (
+            isinstance(other, rp.RationalArray)
+            or isinstance(other, np.ndarray)
+            or isinstance(other, Number)
+        ):
+            return Stencil(self.x, self.w * other)
+        raise ValueError(f"Unsupported operand type {type(other)} for *.")
+
+    def __rmul__(self, other: Union[Number, rp.RationalArray]) -> "Stencil":
+        return self * other
--- tests/test_stencil.py
+++ tests/test_stencil.py
@@ -7,153 +7,160 @@ from stencilpal.stencil import Stencil, StencilError
 
 # === FIXTURES ===
 @pytest.fixture
-def integer_stencil():
-    x = [-2, 0, 2]
-    w = [0.2, 0.5, 0.3]
-    return Stencil(x, w, h=1)
+def empty_stencil():
+    return Stencil([], [])
+
+
+@pytest.fixture
+def float_stencil():
+    return Stencil([-2, 0, 2], [0.2, 0.5, 0.3])
 
 
 @pytest.fixture
 def rational_stencil():
     x = [-2, 0, 2]
     w = rp.rational_array([1, 3, 1], [5, 5, 10])  # [1/5, 3/5, 1/10]
-    return Stencil(x, w, h=1)
+    return Stencil(x, w)
 
 
 # === TEST CASES ===
 
 
-# --- Validation ---
-def test_validation_integer_stencil():
-    stencil = Stencil([1, 2, 3], [0.1, 0.2, 0.3])
-    assert np.all(stencil.x == np.array([1, 2, 3]))
-    assert np.all(stencil.w == np.array([0.1, 0.2, 0.3]))
-    assert stencil.h == 1
+# --- Node Operations ---
+def test_add_node_float_stencil(float_stencil):
+    float_stencil.add_node(-1, 0.4)
+    assert np.all(float_stencil.x == np.array([-2, -1, 0, 2]))
+    assert np.all(float_stencil.w == np.array([0.2, 0.4, 0.5, 0.3]))
 
 
-def test_validation_rational_stencil():
-    w = rp.rational_array([1, 2], [3, 3])  # [1/3, 2/3]
-    stencil = Stencil([0, 1], w)
-    assert isinstance(stencil.w, rp.RationalArray)
-    assert stencil.w[0].numerator == 1
-    assert stencil.w[0].denominator == 3
+def test_add_node_rational_stencil(rational_stencil):
+    rational_stencil.add_node(-1, rp.rational_array(1, 10))
+    assert np.all(rational_stencil.x == np.array([-2, -1, 0, 2]))
+    assert np.all(rational_stencil.w == rp.rational_array([1, 1, 3, 1], [5, 10, 5, 10]))
 
 
-def test_validation_errors():
-    with pytest.raises(ValueError, match="x must be integers."):
-        Stencil([1.0, 2.0], [0.1, 0.2])
-    with pytest.raises(ValueError, match="x and w must be 1D arrays."):
-        Stencil([[1, 2]], [0.1, 0.2])
-    with pytest.raises(ValueError, match="x and w must have the same length."):
-        Stencil([1, 2], [0.1])
+def test_remove_node_float_stencil(float_stencil):
+    float_stencil.remove_node(0)
+    assert np.all(float_stencil.x == np.array([-2, 2]))
+    assert np.all(float_stencil.w == np.array([0.2, 0.3]))
+
+
+def test_remove_node_rational_stencil(rational_stencil):
+    with pytest.raises(
+        NotImplementedError, match="Cannot remove node from rational stencil."
+    ):
+        rational_stencil.remove_node(2)
 
 
 # --- Rescoping ---
-def test_rescope_no_clipping(integer_stencil):
-    integer_stencil.rescope(-3, 3)
-    assert np.all(integer_stencil.x == np.array([-3, -2, -1, 0, 1, 2, 3]))
-    assert np.all(integer_stencil.w == np.array([0, 0.2, 0, 0.5, 0, 0.3, 0]))
-
-
-def test_rescope_with_different_h(integer_stencil):
-    integer_stencil.rescope(-2, 2, h=2)
-    assert np.all(integer_stencil.x == np.array([-2, 0, 2]))
-    assert np.all(integer_stencil.w == np.array([0.2, 0.5, 0.3]))
-
-
-def test_rescope_error_clipping(integer_stencil):
+def test_invalid_rescope_of_empty_stencil_with_h(empty_stencil):
     with pytest.raises(
-        StencilError, match="Rescoping cannot clip existing stencil positions."
+        ValueError,
+        match="Cannot rescope stencil with less than 2 positions without specifying x.",
     ):
-        integer_stencil.rescope(-1, 1)
+        empty_stencil.rescope()
 
 
-def test_rescope_does_not_change_weights_sum(integer_stencil):
-    original_sum = np.sum(np.abs(integer_stencil.w))
-    integer_stencil.rescope(-4, 4)
-    assert np.sum(np.abs(integer_stencil.w)) == original_sum
+def test_trivial_rescope_of_float_stencil_with_h(float_stencil):
+    float_stencil.rescope(h=2)
+    assert np.all(float_stencil.x == np.array([-2, 0, 2]))
+    assert np.all(float_stencil.w == np.array([0.2, 0.5, 0.3]))
 
 
-def test_rescope_rational(rational_stencil):
-    rational_stencil.rescope(-3, 3)
-    assert np.all(
-        rational_stencil.w
-        == rp.rational_array([0, 1, 0, 3, 0, 1, 0], [1, 5, 1, 5, 1, 10, 1])
-    )
-
-
-# --- Trimming ---
-def test_trim_zeros(integer_stencil):
-    integer_stencil.rescope(-3, 3)
-    integer_stencil.trim_zeros()
-    assert np.all(integer_stencil.x == np.array([-2, 0, 2]))
-    assert np.all(integer_stencil.w == np.array([0.2, 0.5, 0.3]))
-
-
-def test_trim_leading_and_trailing_zeros(integer_stencil):
-    integer_stencil.rescope(-3, 3)
-    integer_stencil.trim_leading_and_trailing_zeros()
-    assert np.all(integer_stencil.x == np.array([-2, -1, 0, 1, 2]))
-    assert np.all(integer_stencil.w == np.array([0.2, 0, 0.5, 0, 0.3]))
-
-
-def test_trim_zeros_error_empty_stencil(integer_stencil):
-    integer_stencil.rescope(-3, 3)
-    integer_stencil.w.fill(0)
-    with pytest.raises(ValueError, match="Cannot trim all zeros."):
-        integer_stencil.trim_zeros()
-
-
-# --- Arithmetic Operations ---
-def test_add_stencils(integer_stencil):
-    stencil1 = Stencil([-1, 0, 1], [0.2, 0.5, 0.3])
-    stencil2 = Stencil([-2, 0, 2], [0.1, 0.4, 0.2])
-    result = stencil1 + stencil2
-    assert np.all(result.x == np.array([-2, -1, 0, 1, 2]))
-    assert np.all(result.w == np.array([0.1, 0.2, 0.9, 0.3, 0.2]))
-
-
-def test_multiply_stencil_with_scalar(integer_stencil):
-    result = integer_stencil * 2
-    assert np.all(result.w == np.array([0.4, 1.0, 0.6]))
-
-
-def test_negate_stencil(integer_stencil):
-    result = -integer_stencil
-    assert np.all(result.w == np.array([-0.2, -0.5, -0.3]))
-
-
-# --- Rational Operations ---
-def test_simplify_rational_stencil(rational_stencil):
-    rational_stencil.simplify()
+def test_trivial_rescope_of_ratinal_stencil_with_h(rational_stencil):
+    rational_stencil.rescope(h=2)
+    assert np.all(rational_stencil.x == np.array([-2, 0, 2]))
     assert np.all(rational_stencil.w == rp.rational_array([1, 3, 1], [5, 5, 10]))
 
 
-def test_common_denominator_rational_stencil(rational_stencil):
-    rational_stencil.form_common_denominator()
-    numerators = rational_stencil.w.numerator.tolist()
-    denominators = rational_stencil.w.denominator.tolist()
-    assert numerators == [2, 6, 1]
-    assert all(d == 10 for d in denominators)
+def test_nontrivial_rescope_of_float_stencil_with_h(float_stencil):
+    float_stencil.rescope()
+    assert np.all(float_stencil.x == np.array([-2, -1, 0, 1, 2]))
+    assert np.all(float_stencil.w == np.array([0.2, 0.0, 0.5, 0.0, 0.3]))
 
 
-def test_asnumpy_rational_array_with_mode_numerator(rational_stencil):
-    padded_stencil = rational_stencil.rescope(inplace=False)
-    result = rational_stencil.asnumpy(mode="numerator")
-    assert np.all(result == np.array([2, 0, 6, 0, 1]))
-    assert np.all(rp.rational_array(result, 10) == padded_stencil.w)
+def test_nontrivial_rescope_of_rational_stencil_with_h(rational_stencil):
+    rational_stencil.rescope()
+    assert np.all(rational_stencil.x == np.array([-2, -1, 0, 1, 2]))
+    assert np.all(
+        rational_stencil.w == rp.rational_array([1, 0, 3, 0, 1], [5, 1, 5, 1, 10])
+    )
 
 
-def test_asnumpy_numpy_array_with_mode_numerator(integer_stencil):
+def test_rescope_of_empty_stencil_with_x(empty_stencil):
+    empty_stencil.rescope(np.array([-2, -1, 0, 1, 2]))
+    assert np.all(empty_stencil.x == np.array([-2, -1, 0, 1, 2]))
+    assert np.all(empty_stencil.w == 0)
+
+
+def test_rescope_of_float_stencil_with_x(float_stencil):
+    float_stencil.rescope(np.array([-2, -1, 0, 1, 2]))
+    assert np.all(float_stencil.x == np.array([-2, -1, 0, 1, 2]))
+    assert np.all(float_stencil.w == np.array([0.2, 0.0, 0.5, 0.0, 0.3]))
+
+
+def test_rescope_of_rational_stencil_with_x(rational_stencil):
+    rational_stencil.rescope(np.array([-2, -1, 0, 1, 2]))
+    assert np.all(rational_stencil.x == np.array([-2, -1, 0, 1, 2]))
+    assert np.all(
+        rational_stencil.w == rp.rational_array([1, 0, 3, 0, 1], [5, 1, 5, 1, 10])
+    )
+
+
+def test_invalid_rescope_of_float_stencil_with_x(float_stencil):
     with pytest.raises(
-        ValueError, match="'numerator' mode can only be used with rational stencils."
+        StencilError,
+        match="Rescoping would exclude existing stencil positions with non-zero weights.",
     ):
-        integer_stencil.asnumpy(mode="numerator")
+        float_stencil.rescope(np.array([-1, 0, 1, 2]))
 
 
-def test_asnumpy_any_array_with_mode_float(integer_stencil, rational_stencil):
-    integer_result = integer_stencil.asnumpy(mode="float")
-    assert np.all(integer_result == np.array([0.2, 0, 0.5, 0, 0.3]))
+def test_invalid_rescope_of_rational_stencil_with_x(rational_stencil):
+    with pytest.raises(
+        StencilError,
+        match="Rescoping would exclude existing stencil positions with non-zero weights.",
+    ):
+        rational_stencil.rescope(np.array([-1, 0, 1, 2]))
 
-    rational_result = rational_stencil.asnumpy(mode="float")
-    assert np.all(rational_result == np.array([0.2, 0, 0.6, 0, 0.1]))
+
+# --- Copy ---
+def test_copy_empty_stencil(empty_stencil):
+    stencil_copy = empty_stencil.copy()
+    assert len(stencil_copy.x) == 0
+    assert len(stencil_copy.w) == 0
+    assert stencil_copy is not empty_stencil
+
+
+def test_copy_float_stencil(float_stencil):
+    stencil_copy = float_stencil.copy()
+    assert np.all(stencil_copy.x == float_stencil.x)
+    assert np.all(stencil_copy.w == float_stencil.w)
+    assert stencil_copy is not float_stencil
+
+
+def test_copy_rational_stencil(rational_stencil):
+    stencil_copy = rational_stencil.copy()
+    assert np.all(stencil_copy.x == rational_stencil.x)
+    assert np.all(stencil_copy.w == rational_stencil.w)
+    assert stencil_copy is not rational_stencil
+
+
+# --- Arithmetic Operations ---
+def test_add_rational_stencils():
+    stencil1 = Stencil([-1, 1], rp.rational_array([1, 1], [5, 5]))
+    stencil2 = Stencil([-2, 0, 2], rp.rational_array([1, 1, 1], [5, 5, 5]))
+    result = stencil1 + stencil2
+    assert np.all(result.x == np.array([-2, -1, 0, 1, 2]))
+    assert np.all(result.w == rp.rational_array(1, 5))
+
+
+def test_multiply_rational_stencil_with_scalar(rational_stencil):
+    result = rational_stencil * rp.rational_array(5, 2)
+    assert np.all(result.x == np.array([-2, 0, 2]))
+    assert np.all(result.w == rp.rational_array([1, 3, 1], [2, 2, 4]))
+
+
+def test_negate_rational_stencil(rational_stencil):
+    result = -rational_stencil
+    assert np.all(result.x == rational_stencil.x)
+    assert np.all(result.w == -rational_stencil.w)
